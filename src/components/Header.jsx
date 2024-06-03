681ec0f208f09038e68cd03f2b0f6eef068e841b
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-06-03 15:36:37 +0100

    refactored useEffect, added cleanup on return

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ import React, { useState, useEffect } from 'react'
 
 export default function Header() {
 
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth) 
 
-    useEffect(() => {
-        window.addEventListener('resize', () => {
+    useEffect(() => { // useEffect hook to watch the window width
+        function watchWindowWidth() { // function to watch the window width
             setWindowWidth(window.innerWidth)
-        })
+        }
+
+        window.addEventListener('resize', watchWindowWidth) // add event listener to watch the window width
+
+        return () => { // cleanup function to remove the event listener
+            window.removeEventListener('resize', watchWindowWidth)
+        }
     }, [])
 
     return (
@@ -17,8 +23,6 @@ export default function Header() {
                 <h2>Meme Generator</h2>
             </div>
             <h3>Window Width: {windowWidth}px</h3>
-            
-
         </div>
     )
 }
